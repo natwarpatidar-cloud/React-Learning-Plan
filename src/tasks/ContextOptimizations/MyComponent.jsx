bef9c4746883f5009a5ac6e33e5d8d0d19b66fa3
NatwarPatidar <natwar.patidar@gammaedge.io>
2025-10-27 15:05:45 +0530

    custom reducer with logger and thunk middleware, context optimiztion usng react context api

---
+++ src/tasks/ContextOptimizations/MyComponent.jsx
@@ -0,0 +1,33 @@
+// COntext optimization.. using useContext
+import React, { useContext, useMemo } from 'react';
+
+const MyContext = React.createContext({});
+
+function useSelectedContext(selector) {
+  const contextValue = useContext(MyContext);
+  const selectedValue = useMemo(() => selector(contextValue), [contextValue, selector]);
+  return selectedValue;
+}
+
+function MyComponent() {
+  const username = useSelectedContext(state => state.user.username);
+
+  return (
+    <div>
+      <p>Welcome, {username}!</p>
+    </div>
+  );
+}
+
+function App() {
+  const contextData = {
+    user: { username: 'John Doe', id: 123 },
+    settings: { theme: 'dark', notifications: true }
+  };
+
+  return (
+    <MyContext.Provider value={contextData}>
+      <MyComponent />
+    </MyContext.Provider>
+  );
+}
